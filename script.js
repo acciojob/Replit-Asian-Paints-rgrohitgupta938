5a98b9e8f7be4c55bf32c334d96da4cb63c58c38
Rohit Gupta <49370602+rgrohitgupta938@users.noreply.github.com>
2024-09-03 17:33:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const changeBtn = document.getElementById("change_button");
+const resetBtn = document.getElementById("reset");
+const blockIdInput = document.getElementById("block_id");
+const colorIdInput = document.getElementById("color_id");
+const getItems = document.querySelectorAll(".grid-item");
+
+changeBtn.addEventListener("click", () => {
+  console.log("inside");
+  const blockId = blockIdInput.value;
+  const colorId = colorIdInput.value;
+  console.log(blockIdInput.value, blockIdInput, colorId);
+  getItems.forEach((i) => (i.style.backgroundColor = "transparent"));
+
+  const selitem = document.getElementById(blockId);
+  if (selitem) {
+    selitem.style.backgroundColor = colorId;
+  } else {
+    alert("Invalid Blaock id");
+  }
+});
+resetBtn.addEventListener("click", () => {
+  getItems.forEach((i) => (i.style.backgroundColor = "transparent"));
+});
+
+console.log("HELLO");
